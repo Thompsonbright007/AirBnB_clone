8d36f7016ee342812cb74123cbebc6baacf74b7a
root <root@LOVELY-WANDYTE>
2023-10-15 13:38:35 +0100

    test cases

--- console.py
+++ console.py
@@ -4,26 +4,41 @@
     This is the AirBnB command interface for the AirBnB project that
     inherits from the cmd class
 """
-import cmd, sys
+import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models.__init__ import storage
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class AirBnB_Console(cmd.Cmd):
     """
     An instance AirBnB_Console command
     
-    Args:
+    Attributes:
         intro: a message to welcome the user
         prompt: The text issued on every cmd instance
+        __classes - Contains a dictionary of all classes
     """
     intro = "Welcome to the AirBnB_Clone project"
-    prompt = ">>>>> "
+    prompt = "(hbnb) "
+    __classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                }
 
-    def __init__(self):
+    def emptyline(self):
         """
-        An instance of the AirBnb_Command interface that inherits from
-        the cmd class
+        emptyline - This method called when we have an empty line.
+                    If this method is not overridden,
+                    it repeats the last nonempty command entered.
         """
-        super().__init__()
+        pass
 
     def do_quit(self, line):
         """a function that ends the program interaction"""
